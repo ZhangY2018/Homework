9238abf10adc1e4dffd4a03a95c49bb238fe3825
ZhangY2018 <yzhang@sjnma.org>
2018-02-12 09:42:21 -0600

    update README to include titles

---
+++ hw3/p1.py
@@ -0,0 +1,22 @@
+#integer
+number1 = 10
+number2 = 5
+number3 = number1 + number2
+print(number3)
+
+#floating point
+number1 = 5.0
+number2 = 10.0
+number3 = number1 / number2
+print(number3)
+
+#string
+greeting1 = raw_input(" What is your first name? ")
+greeting2 = raw_input(" What is your last name? ") 
+print("Hello, " + greeting1 +" "+ greeting2)
+
+#boolean
+myMoodIsGood = True
+iPlayedOutside = True
+iHaveAGoodDay = myMoodIsGood + iPlayedOutside
+print(iHaveAGoodDay)
