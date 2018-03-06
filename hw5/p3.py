a8588d07a72972adae3ff18aed1de195eea9787f
ZhangY2018 <yzhang@sjnam.org>
2018-03-05 19:41:49 -0600

    update hw5 to include titles

--- hw5/p3.py
+++ hw5/p3.py
@@ -5,9 +5,8 @@ words=lower.split(',')
 numOfWords=len(words)
 
 if(numOfWords%2 == 1):
-
 	if("llama" or "Llama" in words):
 		print(' '.join(words))
 
 elif((numOfWords%2 == 1) or ((words[0]%2 == 1) and (words[1]%2 == 1) and (words[2]%2 == 1) and (words[3]%2 == 1) and (words[4]%2 == 1))):
-	print("That is add...O_O")
+	print("That is odd...O_O")
