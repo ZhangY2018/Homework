6667c0a347417bbfde914a750c65d0d4b9cd7dcf
ZhangY2018 <yzhang@sjnam.org>
2018-03-19 20:46:18 -0500

    update Homework to include titles

---
+++ hw7/p1.py
@@ -0,0 +1,10 @@
+Sentence = raw_input("Enter any sentec that you want to")
+
+def findUpper(wordsThatAreUpper):
+	wordsThatAreUpper = [upper]
+
+	return wordsThatAreUpper
+
+for(words in wordsThatAreUpper):
+
+	print(Sentencey)
---
+++ hw7/p2.py
@@ -0,0 +1,5 @@
+import csv
+with open('roster.csv' , 'rb')as f:
+	reader = csv.reader(f)
+	for row in reader:
+		print row
